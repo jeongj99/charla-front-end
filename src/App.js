c9a1f8735355148c39c6a6143b2a31fd4e3ad33c
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-01-16 22:39:34 -0300

    Import Chat page component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import About from "./pages/About";
+import Chat from "./pages/Chat";
 
 import './App.css';
 
