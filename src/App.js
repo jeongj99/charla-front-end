849e18390a120dbbc52c4ef49b24562de07e0dd1
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-01-16 22:32:16 -0300

    Import Register page component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Register from "./pages/Register"
 
 import './App.css';
 
