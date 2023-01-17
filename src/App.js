cbeef1b5ae5173ac31573a796cae011fcd00db6b
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-01-16 22:36:07 -0300

    Import About page component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Register from "./pages/Register"
+import Register from "./pages/Register";
+import About from "./pages/About";
 
 import './App.css';
 
