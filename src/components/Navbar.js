4166e2bc7124ea40123be952cf3f8dc87e0909bc
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-01-18 22:16:30 -0300

    Add code to Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,36 @@
+import { Link, NavLink, useNavigate } from "react-router-dom";
+
 import { IconContext } from "react-icons/lib";
 
 export default function Navbar() {
   return (
     <>
       <IconContext.Provider value={{ color: "#fff" }}>
+        <nav className="navbar">
+          <div className="navbar-container container">
+            <Link to="/" className="navbar-logo">
+              Charla
+            </Link>
+            <ul>
+              <li className="nav-item">
+                <NavLink to="/about" className={({ isActive }) => "nav-links" + (isActive ? "activated" : "")} >
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/login" className={({ isActive }) => "nav-links" + (isActive ? "activated" : "")} >
+                  Log In
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/login" className={({ isActive }) => "nav-links" + (isActive ? "activated" : "")} >
+                  Register
+                </NavLink>
+              </li>
+            </ul>
+
+          </div>
+        </nav>
       </IconContext.Provider>
     </>
   );
