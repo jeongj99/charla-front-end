700531341e68fc879799031abb85447d0a932460
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-01-18 22:45:30 -0300

    Finish first version of the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,41 @@
+import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 
 import "./Navbar.css";
+import { FaBars, FaTimes } from "react-icons/fa";
 import { IconContext } from "react-icons/lib";
 
 export default function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(prev => !prev);
+
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <>
       <IconContext.Provider value={{ color: "#fff" }}>
         <nav className="navbar">
           <div className="navbar-container container">
-            <Link to="/" className="navbar-logo">
+            <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
               Charla
             </Link>
-            <ul>
+            <div className="menu-icon" onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </div>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
               <li className="nav-item">
-                <NavLink to="/about" className={({ isActive }) => "nav-links" + (isActive ? "activated" : "")} >
+                <NavLink to="/about" className={({ isActive }) => "nav-links" + (isActive ? "activated" : "")} onClick={closeMobileMenu} >
                   About
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="/login" className={({ isActive }) => "nav-links" + (isActive ? "activated" : "")} >
+                <NavLink to="/login" className={({ isActive }) => "nav-links" + (isActive ? "activated" : "")} onClick={closeMobileMenu} >
                   Log In
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="/login" className={({ isActive }) => "nav-links" + (isActive ? "activated" : "")} >
+                <NavLink to="/login" className={({ isActive }) => "nav-links" + (isActive ? "activated" : "")} onClick={closeMobileMenu} >
                   Register
                 </NavLink>
               </li>
