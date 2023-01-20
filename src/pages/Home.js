7b76841456e12044664600cefb92a6a8c5ef425c
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-01-20 20:33:38 -0300

    Update styling of Navbar and homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
 import Navbar from "../components/Navbar";
 
+import "./Home.css";
+
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <section className="home-container">
-        This is home page!
-      </section>
+      <div className="home-container">
+        <Navbar />
+        <section className="home-section">
+          This is home page!
+        </section>
+      </div>
     </>
   );
 }
