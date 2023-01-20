0362eae51d4067f076cee7ffe44106e7cbbd7554
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-01-19 21:40:42 -0300

    Make outline for chat page and style it with color to visualize

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
+import "./Chat.css";
+
 export default function Chat() {
   const { id } = useParams();
 
   return (
     <>
-      {id ? <div>This is chat page and id is {id}</div> : <div>This is chat page and there is no id</div>}
+      <div className="container">
+        <aside className="sidebar">
+          <div className="sidebar-logo">
+            Charla
+          </div>
+          <div className="sidebar-chats">
+            all the chats
+          </div>
+          <div className="sidebar-profile">
+            Alex Jeong
+          </div>
+        </aside>
+        <section className="chat">
+          <div className="chat-info">
+            John
+          </div>
+          <div className="chat-message">
+            {id ? <h2>This is chat page and id is {id}</h2> : <h2>This is chat page and there is no id</h2>}
+          </div>
+          <div className="chat-input">
+            Type a message
+          </div>
+        </section>
+      </div>
     </>
   );
 }
