4ddb0be08ee6facd3df58de2dd651f233db8750e
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-01-21 22:53:12 -0300

    Delete LoginForm and RegisterForm components

--- src/components/LoginForm.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Form, Button, Input } from "antd";
-
-export default function LoginForm() {
-  return (
-    <div>LoginForm</div>
-  );
-}
--- src/components/RegisterForm.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Form, Button, Input } from "antd";
-
-export default function RegisterForm() {
-  return (
-    <div>RegisterForm</div>
-  )
-}
