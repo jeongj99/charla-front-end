c6835fc78131d65d79ebfef546f4aa63411aa2c3
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-01-23 20:24:47 -0300

    Create Common components for the Login and Register Form

--- src/components/Form/Common.js
+++ src/components/Form/Common.js
@@ -1,5 +1,38 @@
-export default function Common() {
+import classNames from "classnames";
+
+import "./Common.css";
+
+export function CommonContainer(props) {
   return (
-    <div>Common</div>
+    <div className="common-container">{props.children}</div>
+  );
+}
+
+export function CommonForm(props) {
+  return (
+    <form className="common-form-container">{props.children}</form>
+  );
+}
+
+export function CommonLink(props) {
+  const commonLinkClass = classNames({
+    "muted-link": props.muted,
+    "bold-link": props.bold
+  });
+
+  return (
+    <a className={commonLinkClass} href={props.href}>{props.children}</a>
+  );
+}
+
+export function CommonInput(props) {
+  return (
+    <input className="form-input" type={props.type} placeholder={props.placeholder} />
+  );
+}
+
+export function CommonSubmitButton(props) {
+  return (
+    <button className="form-submit">{props.children}</button>
   );
 }
