856fa94a3fbc995748788b125e4276d1998f163f
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-01-22 22:18:03 -0300

    Add some messages to the form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -4,7 +4,12 @@ export default function Form() {
   return (
     <div className="form-container">
       <div className="form-top-container">
-        <div className="backdrop"></div>
+        <div className="backdrop" />
+        <div className="form-header-container">
+          <h2 className="form-header-message">Welcome</h2>
+          <h2 className="form-header-message">Back</h2>
+          <h5 className="form-header-small-message">Please log in to continue</h5>
+        </div>
       </div>
     </div>
   );
