e19abcdd0773c912634383ade4f0e097bfaf4b60
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-01-23 22:24:54 -0300

    Create RegisterForm

--- src/components/Form/RegisterForm.js
+++ src/components/Form/RegisterForm.js
@@ -1,5 +1,23 @@
+import { CommonContainer, CommonForm, CommonLink, CommonInput, CommonSubmitButton } from "./Common";
+import Marginer from "../Marginer";
+
 export default function RegisterForm() {
   return (
-    <div>RegisterForm</div>
+    <CommonContainer>
+      <CommonForm>
+        <CommonInput type="text" placeholder="First Name" />
+        <CommonInput type="text" placeholder="Last Name" />
+        <CommonInput type="text" placeholder="Username" />
+        <CommonInput type="email" placeholder="Email" />
+        <CommonInput type="password" placeholder="Password" />
+        <CommonInput type="password" placeholder="Confirm Password" />
+      </CommonForm>
+      <Marginer direction="vertical" margin={10} />
+      <CommonLink muted href="#">Forgot your password?</CommonLink>
+      <Marginer direction="vertical" margin="1.6em" />
+      <CommonSubmitButton>Log in</CommonSubmitButton>
+      <Marginer direction="vertical" margin="1em" />
+      <CommonLink muted >Don't have an account? <CommonLink bold href="asdfweiufwephf">Register</CommonLink></CommonLink>
+    </CommonContainer>
   );
 }
