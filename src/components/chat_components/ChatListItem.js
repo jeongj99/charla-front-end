900e34580e1f4df94be457838aa1b9c03414fe73
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-01-23 20:29:54 -0500

    Applied blank user photo to chat list item.

--- src/components/chat_components/ChatListItem.js
+++ src/components/chat_components/ChatListItem.js
@@ -4,7 +4,7 @@ export default function ChatListItem(props) {
 
   return (
     <main className="chat-list-item-container">
-      
+      <img className="chat-list-item-user-photo" src="https://ouch-cdn2.icons8.com/HVpVN0Xu75AqsHQ0S7oIwHtbSiwb1pPSkGzQadwHuZo/rs:fit:256:256/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNDk5/Lzk3ODhkMjhiLWZj/NmMtNGYxNS05OTBl/LTkyNTc5Y2VlZmIw/MS5zdmc.png"/>
     </main>
 
   );
