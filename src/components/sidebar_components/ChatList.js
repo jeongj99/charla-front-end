ca0ed7a9a22c850324a7322cb6641cbae1662453
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-01-27 11:08:47 -0500

    New commenting.

--- src/components/sidebar_components/ChatList.js
+++ src/components/sidebar_components/ChatList.js
@@ -6,7 +6,6 @@ export default function ChatList(props) {
 
     useEffect(() => {
     async function fetchData() {
-      //Use post request to create new game in DB, returns response object with information.
       let response = await fetch(`http://localhost:8001/api/chat/list`, {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' },
