a31ca1808b146d09edfefb182967963b25f36d0b
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-01-27 18:09:36 -0500

    Applied CSS to ProfilePic and passed message prop.

--- src/components/sidebar_components/ChatListItem.js
+++ src/components/sidebar_components/ChatListItem.js
@@ -1,13 +1,14 @@
+import { Profiler } from "react";
 import "./ChatListItem.css"
 
 export default function ChatListItem(props) {
 
   return (
     <main className="chat-list-item-container">
-      <img className="chat-list-item-user-photo" src="https://ouch-cdn2.icons8.com/HVpVN0Xu75AqsHQ0S7oIwHtbSiwb1pPSkGzQadwHuZo/rs:fit:256:256/czM6Ly9pY29uczgu/b3"/>
+      <img className="chat-list-item-user-photo" src={props.profilePic}/>
       <div className="chat-list-item-conversation">
         <h3>{props.firstName} {props.lastName}</h3>
-        <p>You: This is a test conversation to see how a conversation would look within the ChatListItem component.</p>
+        <p>{props.message}</p>
         </div>
     </main>
 
