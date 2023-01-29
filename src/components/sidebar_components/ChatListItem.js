44493a45d7d30ee8ebb0911b34c9c09aae62193d
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-01-28 22:08:40 -0500

    Added containers for photo to fill, future change.

--- src/components/sidebar_components/ChatListItem.js
+++ src/components/sidebar_components/ChatListItem.js
@@ -5,9 +5,9 @@ export default function ChatListItem(props) {
 
   return (
     <main className="chat-list-item-container">
-      <div className="chat-list-item-user-photo-container">
+      {/* <div className="chat-list-item-user-photo-container"> */}
       <img className="chat-list-item-user-photo" src={props.profilePic}/>
-      </div>
+      {/* </div> */}
       <div className="chat-list-item-conversation">
         <h3>{props.firstName} {props.lastName}</h3>
         <p>{props.message}</p>
