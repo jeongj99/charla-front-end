4b91cb0a553e38a45a03a5b6900e8c238dc7c8d2
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-01-29 22:50:05 -0300

    Create states and pass them as props to the inputs

--- src/components/Form/RegisterForm.js
+++ src/components/Form/RegisterForm.js
@@ -1,16 +1,27 @@
+import { useState } from "react";
+import axios from "axios";
+
 import { CommonContainer, CommonForm, CommonLink, CommonInput, CommonSubmitButton } from "./Common";
 import Marginer from "../Marginer";
 
 export default function RegisterForm(props) {
+  const [firstNameRegister, setFirstNameRegister] = useState("");
+  const [lastNameRegister, setLastNameRegister] = useState("");
+  const [usernameRegister, setUsernameRegister] = useState("");
+  const [emailRegister, setEmailRegister] = useState("");
+  const [passwordRegister, setPasswordRegister] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
   return (
     <CommonContainer>
       <CommonForm>
-        <CommonInput type="text" placeholder="First Name" />
-        <CommonInput type="text" placeholder="Last Name" />
-        <CommonInput type="text" placeholder="Username" />
-        <CommonInput type="email" placeholder="Email" />
-        <CommonInput type="password" placeholder="Password" />
-        <CommonInput type="password" placeholder="Confirm Password" />
+        <CommonInput type="text" placeholder="First Name" value={firstNameRegister} onChange={setFirstNameRegister} />
+        <CommonInput type="text" placeholder="Last Name" value={lastNameRegister} onChange={setLastNameRegister} />
+        <CommonInput type="text" placeholder="Username" value={usernameRegister} onChange={setUsernameRegister} />
+        <CommonInput type="email" placeholder="Email" value={emailRegister} onChange={setEmailRegister} />
+        <CommonInput type="password" placeholder="Password" value={passwordRegister} onChange={setPasswordRegister} />
+        <CommonInput type="password" placeholder="Confirm Password" value={confirmPassword} onChange={setConfirmPassword} />
       </CommonForm>
       <Marginer direction="vertical" margin={10} />
       <Marginer direction="vertical" margin="1.6em" />
