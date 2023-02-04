9c8800f309d6263624c1d6010071eb0beb2f7f05
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-02-04 10:50:48 -0500

    Created searchUser function for search bar.

--- src/components/sidebar_components/SideBarSearch.js
+++ src/components/sidebar_components/SideBarSearch.js
@@ -1,10 +1,17 @@
+import { useState, useEffect } from 'react';
 import "./SideBarSearch.css"
 
 export default function SideBarSearch(props) {
+  const [searchUser, setSearchUser] = useState(null)
+
+  function SearchForUser(event) {
+    setSearchUser(event.target.value)
+    console.log(event.target.value)
+  }
 
   return (
     <div className="sidebar-search-container">
-      <input type="text" placeholder="Search..."/>
+      <input type="text" placeholder="Search..." onChange={SearchForUser}/>
         <a href="#">
         <i className="fa-solid fa-magnifying-glass"></i>
         </a>
