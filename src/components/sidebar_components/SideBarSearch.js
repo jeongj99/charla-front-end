0af8786e881f69f1ed44ba3bcc951ca5b32239b9
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-02-04 11:31:01 -0500

    Testing putting searchUserState in axios request.

--- src/components/sidebar_components/SideBarSearch.js
+++ src/components/sidebar_components/SideBarSearch.js
@@ -3,13 +3,25 @@ import axios from "axios";
 import "./SideBarSearch.css"
 
 export default function SideBarSearch(props) {
-  const [searchUser, setSearchUser] = useState("")
+  const [searchUser, setSearchUser] = useState("TESTING")
 
   function SearchForUser(event) {
     setSearchUser(event.target.value)
     console.log(event.target.value)
     console.log('Hello from searchSTATE', searchUser)
   }
+
+  useEffect(() => {
+    axios.get('http://localhost:8001/api/searchuser', {
+      params: {
+        searchedUser: searchUser
+      }
+    })
+      .then(response => {
+        console.log('Hello from axios', response.config.params);
+      })
+      .catch(err => console.log(err));
+  }, []);
 
   return (
     <div className="sidebar-search-container">
