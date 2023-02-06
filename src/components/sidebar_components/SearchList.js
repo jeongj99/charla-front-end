b35e392d11e56674097c85be6aecdba7706fd9c6
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-02-06 10:44:39 -0500

    Created .map to iterate over array of usersFound.

--- src/components/sidebar_components/SearchList.js
+++ src/components/sidebar_components/SearchList.js
@@ -1,6 +1,17 @@
+import ChatListItem from './ChatListItem';
 import './SearchList.css';
+
 export default function SearchList(props) {
   console.log('Hello from SEARCHLIST COMP USERSFOUND STATE', props.usersFound)
+
+  const listOfSearchedUsers = props.usersFound.map((searchedUsersObj) => {
+
+    return (
+      <ChatListItem 
+      
+      />
+    )
+  })
 
   return (
     <div className="search-list-container">
