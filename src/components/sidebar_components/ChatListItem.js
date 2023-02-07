78fe5bce2574a9461ae9c7b5666aa85968133e61
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-02-07 09:48:59 -0500

    Incorproated useNavigate on ChatListItem

--- src/components/sidebar_components/ChatListItem.js
+++ src/components/sidebar_components/ChatListItem.js
@@ -1,10 +1,17 @@
 import { Profiler } from "react";
 import "./ChatListItem.css"
+import { useNavigate } from "react-router-dom";
 
 export default function ChatListItem(props) {
+  
+  const navigate = useNavigate();
+  
+  const navigateToChat = function() {
+    navigate("/");
+  };
 
   return (
-    <main className="chat-list-item-container">
+    <main className="chat-list-item-container" onClick={navigateToChat}>
       <div className="chat-list-item-user-photo-container">
       <img className="chat-list-item-user-photo" src={props.profilePic}/>
       </div>
