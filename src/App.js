5fcccc85c1fb8beb224b01f26e606971ebb1e185
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-02-07 21:33:27 -0300

    Import AuthContext to import auth state

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
@@ -5,19 +6,22 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import About from "./pages/About";
 import Chat from "./pages/Chat";
+import AuthContext from "./context/AuthProvider";
 
 import './App.css';
 
 function App() {
+  const { auth, setAuth } = useContext(AuthContext);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={auth ? <Chat /> : <Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/about" element={<About />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/chat/:id" element={<Chat />} />
+        <Route path="/chat" element={auth ? <Chat /> : <Home />} />
+        <Route path="/chat/:id" element={auth ? <Chat /> : <Home />} />
       </Routes>
     </BrowserRouter>
   );
