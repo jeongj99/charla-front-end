214bff82907692d5c82fca9073973925e1989c61
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-02-09 09:24:34 -0500

    Removed header in Chat Messages Component.

--- src/components/chat_components/ChatMessages.js
+++ src/components/chat_components/ChatMessages.js
@@ -17,7 +17,6 @@ export default function ChatMessages(props) {
 
   return (
     <div className="chat-messages">
-      <h2>This is chat page and there is no id</h2>
       <ul>
       {listOfMessages}
     </ul>
