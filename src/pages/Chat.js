725201d8b76a39ac70736eeef63746ddf96d98ab
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-02-09 20:22:58 -0300

    Create logout function with axios and add it to onClick in a button

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,19 +1,24 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from "../api/axios";
-import "./Chat.css";
-import { SiXdadevelopers } from "react-icons/si";
+
 import SideBarSearch from '../components/sidebar_components/SideBarSearch';
 import ChatList from '../components/sidebar_components/ChatList';
 import SearchList from '../components/sidebar_components/SearchList';
 import ChatInput from '../components/chat_components/ChatInput';
 import ChatMessages from '../components/chat_components/ChatMessages';
+import AuthContext from '../context/AuthProvider';
+
+import "./Chat.css";
+import { SiXdadevelopers } from "react-icons/si";
+import { BiLogOut } from "react-icons/bi";
 
 export default function Chat() {
   const { id } = useParams(); //Refers to the /ID in the URL, will be set by UseNavigate in ChatListItem
   const [searchUser, setSearchUser] = useState(""); //State for searching a user in search bar
   const [usersFound, setUsersFound] = useState(""); //State for response of searched user
-  const [convoMessages, setConvoMessages] = useState("") //This state holds all the messages for the selected Chat List Item.
+  const [convoMessages, setConvoMessages] = useState(""); //This state holds all the messages for the selected Chat List Item.
+  const { setAuth } = useContext(AuthContext);
 
   useEffect(() => {
     if (id) {
@@ -51,6 +56,15 @@ export default function Chat() {
     setSearchUser(event.target.value);
   };
 
+  const logout = async () => {
+    try {
+      const response = await axios.post("/api/logout", {});
+      setAuth(response.data.auth);
+    } catch ({ response }) {
+      console.log(response.data.error);
+    }
+  };
+
   return (
     <>
       <div className="chat-container">
@@ -68,6 +82,9 @@ export default function Chat() {
             {searchUser ? <SearchList usersFound={usersFound} /> : <ChatList />}
             <div className="sidebar-profile">
               Alex Jeong with profile pic
+              <button className='logout-button' onClick={logout}>
+                <BiLogOut />
+              </button>
             </div>
           </aside>
           <section className="chat">
