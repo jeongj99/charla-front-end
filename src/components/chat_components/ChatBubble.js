e6d47784ab6705b4d4b3bc5211c3729139c602fc
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-02-10 12:14:55 -0500

    Beta build for floating messages left and right based on user.

--- src/components/chat_components/ChatBubble.js
+++ src/components/chat_components/ChatBubble.js
@@ -1,9 +1,12 @@
 import './ChatBubble.css'
+import classNames from "classnames";
+
 
 export default function ChatBubble(props) {
-
+  let chatBubbleClass = classNames('chat-bubble-user-1', { "chat-bubble-user-2": props.contact % 2 == 0 });
+  
   return (
-    <div className={props.className}>
+    <div className={chatBubbleClass}>
       {props.messages}
     </div>
   )
--- src/components/chat_components/ChatMessages.js
+++ src/components/chat_components/ChatMessages.js
@@ -1,30 +1,15 @@
-import { useContext, useEffect, useState } from "react";
-import AuthContext from "../../context/AuthProvider";
 import './ChatMessages.css';
 import ChatBubble from './ChatBubble';
-import classNames from "classnames";
 
 export default function ChatMessages(props) {
-  const { auth } = useContext(AuthContext);
-  // const [chatMessageAuthIdMatch, setchatMessageAuthIdMatch] = useState(false); //State for a chatMessages corresponding Contact ID.
-  // let controlVariable = null;
 
-  // let chatMessageClass = classNames('chat-bubble', { "chat-bubble-2": controlVariable });
-
-  console.log('HELLO FROM ALL DATA', props.convoMessages.data)
-  
   const listOfMessages = props.convoMessages.data?.map((msgObj) => {
-    // if (msgObj.contact_id === auth.id) {
-    //   controlVariable = true;
-    // } else {
-    //   controlVariable = null;
-    // }
+
+    console.log(props.convoMessages.data)
     
-    console.log("Hello from True or False statement", msgObj.contact_id === auth.id )
     return (
       <ChatBubble
       key={msgObj.id}
-      className={msgObj.contact_id === auth.id ? 'chat-bubble-2' : 'chat-bubble'}
       messages={msgObj.message_text}
       contact={msgObj.contact_id}
       dateTime={msgObj.sent_datetime}
