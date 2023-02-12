5ae60905e982e3b7b5234e2917b8d305b4dfff01
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-02-11 23:36:31 -0300

    Implementing bug fix first try

--- src/components/sidebar_components/ChatList.js
+++ src/components/sidebar_components/ChatList.js
@@ -4,36 +4,54 @@ import './ChatList.css';
 import ChatListItem from "./ChatListItem";
 
 export default function ChatList(props) {
-
   const [chatListState, setChatListState] = useState(false);
 
+  const fetchChatListInfo = async () => {
+    const chatInfo = await axios.get('api/chat/list/message');
+
+    console.log(chatInfo.data);
+
+    for (const element of chatInfo.data) {
+      const response = await axios.get('api/chat/list/profile', {
+        params: {
+          conversationID: element.conversation_id
+        }
+      });
+
+      element.contact_id = response.data.contact_id;
+    }
+    console.log(chatInfo.data);
+
+    setChatListState(chatInfo);
+  };
+
   useEffect(() => {
-    axios.get('api/chat/list')
-      .then(response => {
-        setChatListState(response);
-      })
-      .catch(err => console.log(err));
+    try {
+      fetchChatListInfo();
+    } catch (err) {
+      console.log(err);
+    }
   }, []);
 
   const listOfChats = chatListState.data?.map((chatObj) => {
 
     return (
-      <ChatListItem 
-      key={chatObj.conversation_id}
-      convoID={chatObj.conversation_id}
-      firstName={chatObj.first_name}
-      lastName={chatObj.last_name}
-      message={chatObj.message_text}
-      profilePic={chatObj.profile_photo_url}
+      <ChatListItem
+        key={chatObj.conversation_id}
+        convoID={chatObj.conversation_id}
+        firstName={chatObj.first_name}
+        lastName={chatObj.last_name}
+        message={chatObj.message_text}
+        profilePic={chatObj.profile_photo_url}
       />
     );
   });
 
   return (
     <div className="chat-list-container">
-    <ul>
-      {listOfChats}
-    </ul>
+      <ul>
+        {listOfChats}
+      </ul>
     </div>
 
 
