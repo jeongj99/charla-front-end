4564a077bf313f735fcfff39b8b24e1746591d94
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-02-12 11:13:51 -0500

    Created state and function to listen for enter/message submission.

--- src/components/chat_components/ChatInput.js
+++ src/components/chat_components/ChatInput.js
@@ -3,13 +3,25 @@ import "./ChatInput.css";
 
 export default function ChatInput(props) {
   const [userMessage, setUserMessage] = useState("")
+  const [messageSubmitted, setmessageSubmitted] = useState("")
 
+  // const detectKeyDown = (event) => {
+  //   console.log("Clicked Key", event.key)
+  // }
+  
   const userMessageInput = function(event) {
-    setUserMessage(event.target.value)
-    console.log(userMessage);
+      setUserMessage(event.target.value)
+      console.log(userMessage);
   }
 
+  const handleKeyDown = function(event) {
+    if (event.key === 'Enter') {
+      console.log('Your handle key function is working')
+    }
+  }
+
+
   return (
-    <textarea className="text-area" placeholder="Start a Charla!" onChange={userMessageInput}></textarea>
+    <textarea className="text-area" placeholder="Start a Charla!" onChange={userMessageInput} onKeyDown={handleKeyDown}></textarea>
   );
 }
