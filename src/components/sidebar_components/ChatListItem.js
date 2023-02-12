e137f11396697b61b4c1678306a3f0b2544f3390
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-02-12 13:43:22 -0300

    Add conditional to show if message should appear with 'You:' or not before the message

--- src/components/sidebar_components/ChatListItem.js
+++ src/components/sidebar_components/ChatListItem.js
@@ -1,10 +1,10 @@
 import { Profiler, useEffect, useState } from "react";
-import "./ChatListItem.css"
+import "./ChatListItem.css";
 import { useNavigate } from "react-router-dom";
 
 export default function ChatListItem(props) {
   // const [conversationSelected, setConversationSelected] = useState("")
-  
+
   // useEffect(() => {
   //   axios.get(`api/chat`, {
   //     params: {
@@ -15,25 +15,27 @@ export default function ChatListItem(props) {
   //     console.log('Hello from axios fro NEW CONVERSATION', response.data);
   //   })
   //   .catch(err => console.log(err));
-    
+
   // }, []);
-  
+
   const navigate = useNavigate();
 
   const navigateToChat = function() {
     navigate(`/chat/${props.convoID}`);
     // setConversationSelected(props.convoID);
   };
-  
+
   return (
     <main className="chat-list-item-container" onClick={navigateToChat}>
       <div className="chat-list-item-user-photo-container">
-      <img className="chat-list-item-user-photo" src={props.profilePic}/>
+        <img className="chat-list-item-user-photo" src={props.profilePic} />
       </div>
       <div className="chat-list-item-conversation">
         <h3>{props.firstName} {props.lastName}</h3>
-        <p>{props.message}</p>
-        </div>
+        {
+          props.profileID === props.messageOwnerID ? <p>{props.message}</p> : <p>You: {props.message}</p>
+        }
+      </div>
     </main>
 
   );
