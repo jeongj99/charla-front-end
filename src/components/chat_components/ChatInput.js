e7b4a65b61d188609d852a5a817e7cd00cddc3a0
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-02-12 10:46:15 -0500

    Created state and event listen for keyboard input.

--- src/components/chat_components/ChatInput.js
+++ src/components/chat_components/ChatInput.js
@@ -1,8 +1,15 @@
+import { useState } from "react";
 import "./ChatInput.css";
 
 export default function ChatInput(props) {
+  const [userMessage, setUserMessage] = useState("")
+
+  const userMessageInput = function(event) {
+    setUserMessage(event.target.value)
+    console.log(userMessage);
+  }
 
   return (
-    <textarea className="text-area" placeholder="Start a Charla!"></textarea>
+    <textarea className="text-area" placeholder="Start a Charla!" onChange={userMessageInput}></textarea>
   );
 }
