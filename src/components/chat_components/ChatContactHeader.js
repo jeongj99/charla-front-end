487475185ff4a40d05d36ffe8d1d063ca155ac3b
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-02-14 17:41:03 -0500

    Added comments explaining new state.

--- src/components/chat_components/ChatContactHeader.js
+++ src/components/chat_components/ChatContactHeader.js
@@ -3,7 +3,7 @@ import './ChatContactHeader.css'
 
 export default function ChatContactHeader(props) {
   const { chatListState, id } = props;
-  const [contactInfoForHeader, setContactInfoForHeader] = useState("")
+  const [contactInfoForHeader, setContactInfoForHeader] = useState("") //This state will hold the contact object that matches the current ID/ChatListItem selected
 
     // for (const contactObject of chatListState.data) {
     //   if (contactObject.conversation_id === id) {
