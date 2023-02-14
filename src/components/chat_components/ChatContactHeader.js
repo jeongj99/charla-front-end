4b77aa0680bf50131e2154a5bc7b43564d3b16a3
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-02-14 17:39:33 -0500

    Started building for loop to iterate over array of contact objects in chatListState.

--- src/components/chat_components/ChatContactHeader.js
+++ src/components/chat_components/ChatContactHeader.js
@@ -1,14 +1,17 @@
+import { useState } from 'react';
 import './ChatContactHeader.css'
 
 export default function ChatContactHeader(props) {
   const { chatListState, id } = props;
+  const [contactInfoForHeader, setContactInfoForHeader] = useState("")
 
-  for (let object of chatListState.data) {
-    if (object.conversation_id === id) {
+    // for (const contactObject of chatListState.data) {
+    //   if (contactObject.conversation_id === id) {
+    //     setContactInfoForHeader(contactObject)
+    //     console.log('Hello from your contact object that should match the id you are on', contactObject)
+    //   }
+    // }
 
-    }
-
-  }
   console.log('Hello from all the info inside your chat contact header', chatListState.data)
 
   return (
