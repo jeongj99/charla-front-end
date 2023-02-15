150fb691da8565530fc4c8757f06a4f97ddcdc4c
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-02-15 14:11:25 -0500

    Added logic to prevent loop unless chat list state is not null.

--- src/components/chat_components/ChatContactHeader.js
+++ src/components/chat_components/ChatContactHeader.js
@@ -5,12 +5,18 @@ export default function ChatContactHeader(props) {
   const { chatListState, id } = props;
   const [contactInfoForHeader, setContactInfoForHeader] = useState("") //This state will hold the contact object that matches the current ID/ChatListItem selected
 
-    // for (const contactObject of chatListState.data) {
-    //   if (contactObject.conversation_id === id) {
-    //     setContactInfoForHeader(contactObject)
-    //     console.log('Hello from your contact object that should match the id you are on', contactObject)
-    //   }
-    // }
+  if (!chatListState) {
+    return;
+  } else {
+    for (const contactObject of chatListState.data) {
+      console.log('Hello from your contact object that should match the id you are on', typeof contactObject)
+      console.log('Hello from your ID PARAMS IN THE HEADER', typeof Number(id))
+      if (contactObject.contact.id === Number(id)) {
+        // setContactInfoForHeader(contactObject.contact)
+        (console.log('converting ID to number worked and it now matches id in object'))
+      }
+    }
+  }
 
   // console.log('Hello from all the info inside your chat contact header', chatListState.data)
 
