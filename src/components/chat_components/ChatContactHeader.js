b8535ab68f4f3ffec6eb0d21643f452fec761c61
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-02-15 14:47:51 -0500

    Code clean up in contact header.

--- src/components/chat_components/ChatContactHeader.js
+++ src/components/chat_components/ChatContactHeader.js
@@ -1,32 +1,30 @@
-import { useState } from 'react';
 import './ChatContactHeader.css'
 
 export default function ChatContactHeader(props) {
   const { chatListState, id } = props;
   let contactInfoVariable = "";
-  // const [contactInfoForHeader, setContactInfoForHeader] = useState("") //This state will hold the contact object that matches the current ID/ChatListItem selected
 
   if (!chatListState) {
     return;
   } else {
     for (const contactObject of chatListState.data) {
-      // console.log('Hello from your contact objects conversation id', contactObject.conversation_id)
       if (contactObject.conversation_id === Number(id)) {
-        // console.log('This is the contact object that matched params id', contactObject.conversation_id, id)
         contactInfoVariable = contactObject.contact;
-        // (console.log('converting ID to number worked and it now matches id in object'))
       }
     }
   }
 
   console.log('HELLO FROM YOUR NEW VARIABLE SIR', contactInfoVariable)
 
-
-  // console.log('Hello from all the info inside your chat contact header', chatListState.data)
-
   return (
-    <div className="chat-contact-header">
+    <div className="chat-contact-header-container">
+      {/* <div className='chat-contact-header-contact-image-container'> */}
+      {/* { contactInfoVariable.contact.profile_photo_url ? <img className='chat-contact-header-contact-image'></img> : <img className='chat-contact-header-contact-image'></img>} */}
+      {/* <img className='chat-contact-header-contact-image'>{contactInfoVariable.contact.profile_photo_url}</img> */}
+      {/* </div> */}
+      <div className='chat-contact-header-contact-name'>
       {contactInfoVariable.first_name} {contactInfoVariable.last_name}
+      </div>
   </div>
   );
 }
