edfa7c6914bd179b35f041d817c30f87b32e55d4
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-02-15 14:50:20 -0500

    Refactored code and added more variables to hold contact info for HTML.

--- src/components/chat_components/ChatContactHeader.js
+++ src/components/chat_components/ChatContactHeader.js
@@ -2,19 +2,21 @@ import './ChatContactHeader.css'
 
 export default function ChatContactHeader(props) {
   const { chatListState, id } = props;
-  let contactInfoVariable = "";
+  let contactHeaderFirstName = "";
+  let contactHeaderLastName = "";
 
   if (!chatListState) {
     return;
   } else {
     for (const contactObject of chatListState.data) {
       if (contactObject.conversation_id === Number(id)) {
-        contactInfoVariable = contactObject.contact;
+        contactHeaderFirstName = contactObject.contact.first_name;
+        contactHeaderLastName = contactObject.contact.last_name
       }
     }
   }
 
-  console.log('HELLO FROM YOUR NEW VARIABLE SIR', contactInfoVariable)
+  // console.log('HELLO FROM YOUR NEW VARIABLE SIR', contactInfoVariable)
 
   return (
     <div className="chat-contact-header-container">
@@ -23,7 +25,7 @@ export default function ChatContactHeader(props) {
       {/* <img className='chat-contact-header-contact-image'>{contactInfoVariable.contact.profile_photo_url}</img> */}
       {/* </div> */}
       <div className='chat-contact-header-contact-name'>
-      {contactInfoVariable.first_name} {contactInfoVariable.last_name}
+      {contactHeaderFirstName} {contactHeaderLastName}
       </div>
   </div>
   );
