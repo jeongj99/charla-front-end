faeab7ec14dc69464969129c5eeb38920336a0ec
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-02-19 13:41:14 -0500

    Added new class name so that if message is from admin, chat bubble centered.

--- src/components/chat_components/ChatBubble.js
+++ src/components/chat_components/ChatBubble.js
@@ -3,7 +3,7 @@ import classNames from "classnames";
 
 
 export default function ChatBubble(props) {
-  let chatBubbleClass = classNames('chat-bubble-user-1', { "chat-bubble-user-2": props.contact === props.userID });
+  let chatBubbleClass = classNames('chat-bubble-user-1', { "chat-bubble-user-2": props.contact === props.userID, "chat-bubble-admin-message": props.contact === 5 });
   
   return (
     <div className={chatBubbleClass}>
