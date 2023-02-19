5c18a244a0adf9bea85dd1de0b07e333c30f6340
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-02-19 13:43:02 -0500

    Added comments with regards to future of Admin Id.

--- src/components/chat_components/ChatBubble.js
+++ src/components/chat_components/ChatBubble.js
@@ -3,7 +3,7 @@ import classNames from "classnames";
 
 
 export default function ChatBubble(props) {
-  let chatBubbleClass = classNames('chat-bubble-user-1', { "chat-bubble-user-2": props.contact === props.userID, "chat-bubble-admin-message": props.contact === 5 });
+  let chatBubbleClass = classNames('chat-bubble-user-1', { "chat-bubble-user-2": props.contact === props.userID, "chat-bubble-admin-message": props.contact === 5 }); //Important note, upon deployment if database is adjsuted and admin user is changed, we must change this number.
   
   return (
     <div className={chatBubbleClass}>
