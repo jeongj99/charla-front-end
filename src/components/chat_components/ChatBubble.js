8d0cdf9db84e4466de377e2ac351f40ba4ac06d4
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-03-05 17:52:13 -0300

    Fix chat bubble bug

--- src/components/chat_components/ChatBubble.js
+++ src/components/chat_components/ChatBubble.js
@@ -1,13 +1,16 @@
-import './ChatBubble.css'
+import './ChatBubble.css';
 import classNames from "classnames";
 
 
 export default function ChatBubble(props) {
+  let chatBubbleContainerClass = classNames('chat-bubble-container-1', { "chat-bubble-container-2": props.contact === props.userID, "chat-bubble-admin-container": props.contact === 5 }); //Important note, upon deployment if database is adjsuted and admin user is changed, we must change this number.
   let chatBubbleClass = classNames('chat-bubble-user-1', { "chat-bubble-user-2": props.contact === props.userID, "chat-bubble-admin-message": props.contact === 5 }); //Important note, upon deployment if database is adjsuted and admin user is changed, we must change this number.
-  
+
   return (
-    <div className={chatBubbleClass}>
-      {props.messages}
+    <div className={chatBubbleContainerClass}>
+      <div className={chatBubbleClass}>
+        {props.messages}
+      </div>
     </div>
-  )
+  );
 }
