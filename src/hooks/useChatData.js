0b44b414d94578706ccfe0830c6822623bff29e7
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-03-22 20:22:00 -0300

    Finish the fetchData with the condition to get messages if id is present

--- src/hooks/useChatData.js
+++ src/hooks/useChatData.js
@@ -1,21 +1,31 @@
 import { useState, useEffect } from "react";
 import axios from "../api/axios";
 
-export default function useChatData() {
+export default function useChatData(id) {
   const [state, setState] = useState({
     conversations: [],
-    messeges: []
+    messages: []
   });
 
-  const fetchChatData = async () => {
-    const conversationsResult = await axios.get("api/datatest");
+  const fetchChatData = async (id) => {
+    const conversationsResult = await axios.get("api/conversations");
     setState(prev => ({ ...prev, conversations: conversationsResult.data }));
+
+    if (id) {
+      const messagesResult = await axios.get("api/messages", {
+        params: {
+          id
+        }
+      });
+      console.log(messagesResult);
+      setState(prev => ({ ...prev, messages: messagesResult.data }));
+    }
   };
 
   // On page load, it does all those GET requests to those paths and set the states with the data obtained from those GET Requests
   useEffect(() => {
-    fetchChatData();
-  }, []);
+    fetchChatData(id);
+  }, [id]);
 
   return {
     state
