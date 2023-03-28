ad60ba4cc2bd3187c00319711739ea662b0b50d1
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-03-27 22:55:15 -0300

    retrieve new props

--- src/components/sidebar_components/SideBarSearch.js
+++ src/components/sidebar_components/SideBarSearch.js
@@ -2,11 +2,10 @@ import { useState, useEffect } from 'react';
 import axios from "axios";
 import "./SideBarSearch.css";
 
-export default function SideBarSearch(props) {
-
+export default function SideBarSearch({ value, onChange }) {
   return (
     <div className="sidebar-search-container">
-      <input type="text" placeholder="Search..." value={props.searchUser} onChange={props.SearchForUser} />
+      <input type="text" placeholder="Search..." value={value} onChange={event => onChange(event.target.value)} />
       <a href="#">
         <i className="fa-solid fa-magnifying-glass"></i>
       </a>
