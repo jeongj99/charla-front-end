81c1b1b17c9c07e9395e22ec5b755cc04f166c1d
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-03-29 18:38:23 -0300

    Create new state called loggedInUser

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -5,12 +5,14 @@ const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState(false);
+  const [loggedInUser, setLoggedInUser] = useState(null);
   const [isReady, setIsReady] = useState(false);
 
   const authenticate = async () => {
     try {
       const response = await axios.post('api/authenticate', {});
       setAuth(response.data.authenticated);
+      setLoggedInUser(response.data.contact);
     } catch (error) {
       console.log(error);
       setAuth(false);
@@ -23,7 +25,7 @@ export const AuthProvider = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ auth, setAuth }}>
+    <AuthContext.Provider value={{ auth, setAuth, loggedInUser, setLoggedInUser }}>
       {isReady && children}
     </AuthContext.Provider >
   );
