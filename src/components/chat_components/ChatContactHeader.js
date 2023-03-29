e2b02f65f24756c5a1e2a5ea1151945197e26e2e
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-03-29 19:22:38 -0300

    Update size of profile pic in contact header

--- src/components/chat_components/ChatContactHeader.js
+++ src/components/chat_components/ChatContactHeader.js
@@ -1,23 +1,6 @@
 import './ChatContactHeader.css';
 
 export default function ChatContactHeader({ contactInfo }) {
-  // const { chatListState, id } = props;
-  // let contactHeaderFirstName = "";
-  // let contactHeaderLastName = "";
-  // let contactHeaderPic = "";
-
-  // if (!chatListState) {
-  //   return;
-  // } else {
-  //   for (const contactObject of chatListState.data) {
-  //     if (contactObject.conversation_id === Number(id)) {
-  //       contactHeaderFirstName = contactObject.contact.first_name;
-  //       contactHeaderLastName = contactObject.contact.last_name;
-  //       contactHeaderPic = contactObject.contact.profile_photo_url;
-  //     }
-  //   }
-  // }
-
   return (
     <div className="chat-contact-header-container">
       <div className='chat-contact-header-contact-image-container'>
