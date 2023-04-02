d08b0f3cbc338065bf1210a61b428db20fff0552
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-04-01 22:45:28 -0300

    remove unnecessary imports

--- src/components/sidebar_components/SidebarProfile.js
+++ src/components/sidebar_components/SidebarProfile.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from 'react';
+import { useContext } from 'react';
 import { useNavigate } from "react-router-dom";
 import axios from "../../api/axios";
 import socket from "../../socket";
