18e8a02e991cb79da229e6526222fcd580e687b4
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-04-02 00:24:24 -0400

    Update the searchListItemOnClick prop so it passes the arguments needed

--- src/components/sidebar_components/SearchListItem.js
+++ src/components/sidebar_components/SearchListItem.js
@@ -2,7 +2,7 @@ import "./SearchListItem.css";
 
 export default function SearchListItem(props) {
   return (
-    <main className="search-list-item-container" onClick={() => props.searchListItemOnClick(props.contactID)}>
+    <main className="search-list-item-container" onClick={() => props.searchListItemOnClick(props.contactID, props.firstName, props.lastName)}>
       <div className="search-list-item-user-photo-container">
         <img className="search-list-item-user-photo" alt='profile-pic' src={props.profilePic} />
       </div>
