21674c63cc26604293ec39f1ee9686f357046c8a
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-04-02 14:47:40 -0400

    feat: Added sample photo to home page design to discuss home page layout.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,11 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import classNames from "classnames";
-
 import Navbar from "../components/Navbar";
 import Form from "../components/Form";
 import AuthContext from '../context/AuthProvider';
-
 import "./Home.css";
-
+import img1 from "../images/Sample-phone.png"
 export default function Home() {
   const { auth } = useContext(AuthContext);
 
@@ -24,8 +22,9 @@ export default function Home() {
         <Navbar />
         <section className={homeSectionClass}>
           <div className="home-section-left">
-            <h1>Charla</h1>
-            <p>Experience the power of online conversation with Charla</p>
+            <h1 className="home-section-brand-name">Charla</h1>
+            <img src={img1} alt="" />
+            <p className="home-section-slogan">Experience the power of online conversation with Charla!</p>
           </div>
           <div className="home-section-right">
             {auth ? <button onClick={() => navigate("/chat")}>Go to your chats </button> : <Form />}
