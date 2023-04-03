11adefc302b2f4b1536db344c552ca3f7e314cb0
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-04-03 13:21:02 -0400

    Update css

--- src/components/chat_components/ChatInput.js
+++ src/components/chat_components/ChatInput.js
@@ -101,7 +101,7 @@ export default function ChatInput({ convoID, value, onChange, handleKeyDown }) {
   return (
     <div className="chat-input-container">
       <form className="message-form" onSubmit={event => event.preventDefault}>
-        <textarea className="text-area" placeholder="Start a Charla!" value={value} maxLength="2000" onChange={event => onChange(event.target.value)} onKeyDown={handleKeyDown} />
+        <textarea className="text-area" placeholder="Start a Charla!" value={value} maxLength="2000" onChange={event => onChange(event.target.value)} onKeyDown={event => handleKeyDown(event, convoID)} />
       </form>
     </div>
   );
