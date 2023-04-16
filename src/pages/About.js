1403967db51c30beb009cf09a5d04163675e9c9a
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-04-16 10:55:25 -0400

    feat: Added and formatted about us page.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,7 @@
 import Navbar from "../components/Navbar";
+import "./About.css";
+import alexPhoto from "../images/Alex-Photo.jpeg";
+import jordanPhoto from "../images/Jordan-Photo.jpeg";
 
 export default function About() {
   return (
@@ -6,7 +9,22 @@ export default function About() {
       <div className="about-container">
         <Navbar />
         <section className="about-section">
-          This is about page!
+          <h1 className="page-title">Meet the Creators!</h1>
+          <div className="about-us-container">
+            <div className="alex-container">
+              <h1 className="alex-header">Jae Yun Jeong</h1>
+              <img className="alex-photo" src={alexPhoto} alt="" />
+              <p>Lorem ipsum dolor sit amet, te nullam disputationi has, nonumy melius assentior eu vel. Qui ei nibh appellantur neglegentur, quo magna accusata theophrastus cu. An mei libris equidem. Malis blandit his ad, sed ad facer fabellas, id sit epicurei consetetur voluptatibus. In vim everti tincidunt contentiones. Vix aliquam sanctus ut, ei duo alii facete dolorem.
+                Duo at vide nonumy quidam. Nisl mucius ut vix, ei pri regione virtute mediocrem. Audire labores offendit usu an. Eu has posse maluisset, id eos alii deleniti tincidunt, aperiri reformidans vel no. Eu quis voluptua eum, et error convenire has.</p>
+            </div>
+            <div className="jordan-container">
+              <h1 className="jordan-header">Jordan Guerrero Martinez</h1>
+              <img className="jordan-photo" src={jordanPhoto} alt="" />
+              <p>Lorem ipsum dolor sit amet, te nullam disputationi has, nonumy melius assentior eu vel. Qui ei nibh appellantur neglegentur, quo magna accusata theophrastus cu. An mei libris equidem. Malis blandit his ad, sed ad facer fabellas, id sit epicurei consetetur voluptatibus. In vim everti tincidunt contentiones. Vix aliquam sanctus ut, ei duo alii facete dolorem.
+
+                Duo at vide nonumy quidam. Nisl mucius ut vix, ei pri regione virtute mediocrem. Audire labores offendit usu an. Eu has posse maluisset, id eos alii deleniti tincidunt, aperiri reformidans vel no. Eu quis voluptua eum, et error convenire has.</p>
+            </div>
+          </div>
         </section>
       </div >
     </>
