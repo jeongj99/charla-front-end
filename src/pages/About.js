e13e84646fe23c47dd2f4f6f9bf938d1017c69d7
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-04-16 11:03:49 -0400

    style: added css styles for creator description boxes.

--- src/pages/About.js
+++ src/pages/About.js
@@ -14,13 +14,13 @@ export default function About() {
             <div className="alex-container">
               <h1 className="alex-header">Jae Yun Jeong</h1>
               <img className="alex-photo" src={alexPhoto} alt="" />
-              <p>Lorem ipsum dolor sit amet, te nullam disputationi has, nonumy melius assentior eu vel. Qui ei nibh appellantur neglegentur, quo magna accusata theophrastus cu. An mei libris equidem. Malis blandit his ad, sed ad facer fabellas, id sit epicurei consetetur voluptatibus. In vim everti tincidunt contentiones. Vix aliquam sanctus ut, ei duo alii facete dolorem.
+              <p className="alex-description">Lorem ipsum dolor sit amet, te nullam disputationi has, nonumy melius assentior eu vel. Qui ei nibh appellantur neglegentur, quo magna accusata theophrastus cu. An mei libris equidem. Malis blandit his ad, sed ad facer fabellas, id sit epicurei consetetur voluptatibus. In vim everti tincidunt contentiones. Vix aliquam sanctus ut, ei duo alii facete dolorem.
                 Duo at vide nonumy quidam. Nisl mucius ut vix, ei pri regione virtute mediocrem. Audire labores offendit usu an. Eu has posse maluisset, id eos alii deleniti tincidunt, aperiri reformidans vel no. Eu quis voluptua eum, et error convenire has.</p>
             </div>
             <div className="jordan-container">
               <h1 className="jordan-header">Jordan Guerrero Martinez</h1>
               <img className="jordan-photo" src={jordanPhoto} alt="" />
-              <p>Lorem ipsum dolor sit amet, te nullam disputationi has, nonumy melius assentior eu vel. Qui ei nibh appellantur neglegentur, quo magna accusata theophrastus cu. An mei libris equidem. Malis blandit his ad, sed ad facer fabellas, id sit epicurei consetetur voluptatibus. In vim everti tincidunt contentiones. Vix aliquam sanctus ut, ei duo alii facete dolorem.
+              <p className="jordan-description">Lorem ipsum dolor sit amet, te nullam disputationi has, nonumy melius assentior eu vel. Qui ei nibh appellantur neglegentur, quo magna accusata theophrastus cu. An mei libris equidem. Malis blandit his ad, sed ad facer fabellas, id sit epicurei consetetur voluptatibus. In vim everti tincidunt contentiones. Vix aliquam sanctus ut, ei duo alii facete dolorem.
 
                 Duo at vide nonumy quidam. Nisl mucius ut vix, ei pri regione virtute mediocrem. Audire labores offendit usu an. Eu has posse maluisset, id eos alii deleniti tincidunt, aperiri reformidans vel no. Eu quis voluptua eum, et error convenire has.</p>
             </div>
